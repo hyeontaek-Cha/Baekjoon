3ac784c3ae4faa857d50860ef41f4873dd55fee2
hyeontaek-Cha <84235819+hyeontaek-Cha@users.noreply.github.com>
2022-09-08 15:51:16 +0900

    Update LeapYear.java
    윤년 문제 코드 가독성 개선

--- Stage2/src/No_2753/LeapYear.java
+++ Stage2/src/No_2753/LeapYear.java
@@ -10,7 +10,11 @@ public class LeapYear {
 		System.out.print("");
 		int year = sc.nextInt();
 		
-		if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+		int year_4_mod = year % 4;
+		int year_100_mod = year % 100;
+		int year_400_mod = year % 400;
+		
+		if (year_4_mod == 0 && year_100_mod != 0 || year_400_mod == 0)
 			System.out.println("1");
 		else
 			System.out.println("0");
