65d0d7c623b75b7b07905a9fb6e179f4d94ca036
hyeontaek-Cha <84235819+hyeontaek-Cha@users.noreply.github.com>
2022-10-04 23:49:38 +0900

    Create Average.java

---
+++ Stage4/src/No_1546/Average.java
@@ -0,0 +1,29 @@
+package No_1546;
+
+import java.util.Scanner;
+
+public class Average {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int cnt = sc.nextInt();
+
+		double score[] = new double[cnt];
+		double max = score[0];
+		double sum = 0;
+		double result = 0;
+		for (int i = 0; i < cnt; i++) {
+			score[i] = sc.nextInt();
+		}
+		for (int i = 0; i < score.length; i++) {
+			if (max < score[i]) {
+				max = score[i];
+			}
+			sum += score[i];
+
+			result = (sum / max * 100) / cnt;
+		}
+		System.out.println(result);
+	}
+}
