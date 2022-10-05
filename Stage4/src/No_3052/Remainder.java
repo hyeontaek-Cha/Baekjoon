b07348013540597edb30f9f8144dbc8cc2dc4d76
hyeontaek-Cha <84235819+hyeontaek-Cha@users.noreply.github.com>
2022-10-05 14:09:08 +0900

    Update Remainder.java

--- Stage4/src/No_3052/Remainder.java
+++ Stage4/src/No_3052/Remainder.java
@@ -10,15 +10,13 @@ public class Remainder {
 		Scanner sc = new Scanner(System.in);
 		int[] n = new int[10];
 		int[] result = n;
+		Set set = new HashSet();
 		for (int i = 0; i < n.length; i++) {
 			n[i] = sc.nextInt();
 			n[i] %= 42;
 			result[i] = n[i];
-		}
-		Set set = new HashSet();
-		for (int i = 0; i < result.length; i++) {
 			set.add(result[i]);
 		}
-		System.out.print(set.size());
+		System.out.println(set.size());
 	}
 }
