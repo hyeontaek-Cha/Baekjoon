4372e1ddefd5bb2bf50876ef6d62cccebfb93402
hyeontaek-Cha <84235819+hyeontaek-Cha@users.noreply.github.com>
2022-11-16 22:41:26 +0900

    Create N_Sum.java

---
+++ Stage5/src/No_15596/N_Sum.java
@@ -0,0 +1,13 @@
+package No_15596;
+
+public class N_Sum {
+	public class Test {
+		long sum(int[] a) {
+			long ans = 0;
+			for (int i = 0; i < a.length; i++) {
+				ans += a[i];
+			}
+			return ans;
+		}
+	}
+}
